2cb3ad67b0d2114ac72a84521443a88995f39f93
raymondodinzeo <houghton535@btinternet.com>
2024-12-01 18:28:42 +0000

    Update espNow_testRecieve.ino

--- espNow_testRecieve.ino
+++ espNow_testRecieve.ino
@@ -1,10 +1,17 @@
 //espNow recieve test code to flash on-board LED of peer while in range
+//Comment/uncomment lines as required
 //Code based on Random Nert Tutorials
-//https://randomnerdtutorials.com/esp-now-esp32-arduino-ide/
-//NodeMCU #3 
-#include <espnow.h>  //Libraries for esp8266
-#include <ESP8266WiFi.h>
-int ledPin = 2; //For NodeMCU blue LED on module
+//https://randomnerdtutorials.com/esp-now-esp32-arduino-ide/ 
+//==============================================
+//#include <espnow.h>  //Libraries for esp8266
+//#include <ESP8266WiFi.h>
+//================================================
+//================================================
+#include <esp_now.h> //Libraries for esp32, uncomment as necessary 
+#include <WiFi.h>
+//================================================
+//int ledPin = 8; //For esp32C3 super mini blue LED on module
+int ledPin = 16; //For Node MCU LED on module
 // Structure example to receive data
 // Must match the sender structure
 typedef struct struct_message {
@@ -33,13 +40,22 @@ void setup() {
   
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-
-  // Init ESP-NOW
-  if (esp_now_init() != ERR_OK) {
+//======================================
+//For esp8266
+//Init ESP-NOW   
+// if (esp_now_init() != ERR_OK) {
+//  Serial.println("Error initializing ESP-NOW");
+//   return;
+//}
+  //=====================================
+  //=====================================
+  //For esp32
+  if (esp_now_init() != ESP_OK)
+  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
+  //======================================
   // Once ESPNow is successfully Init, we will register for recv CB to
   // get recv packer info
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
